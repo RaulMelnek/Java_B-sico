3f2bfcb63048e82bc41e6d6fdbbd782a511077fa
RaulMelnek <miguel.rauldesouzasantos@gmail.com>
2025-09-23 11:34:51 -0300

    Cria a classe 'metodos' no pacote 'estudo.JavaBasico.metodos'

---
+++ JavaBasico_Variaveis/src/estudo/JavaBasico/metodos/metodos.java
@@ -0,0 +1,35 @@
+package estudo.JavaBasico.metodos;
+
+public class metodos {
+            public double somar(int num1, int num2){}
+            //LÓGICA - FINALIDADE DO MÉTODO
+            // tem return pq não é void
+            //pois retornara um resultado
+            retrn ...;
+
+            public void imprimir (String texto){
+            }
+            //LÓGICA - FINALIDADE DO MÉTODO
+            //aqui não tem return pq é void
+            //pois não retornara nenhum resultado
+
+            private void metodoPrivado(){
+            //este metodo so pode ser 
+            //chamado dentro desta classe
+            }
+
+            public void gravarCiente (String nome, String cpf){
+            //finalidade de gravar informações do cliente,
+            //por que não criar um objeto
+            //cliente e passar como parâmetro?
+            }
+
+            //pode-se criar uma classe cliente e
+            //criar um método que receba um objeto cliente 
+
+            public void gravarCliente (Cliente cliente){}
+                //ou
+            public void gravar (Cliente cliente){}
+            //finalidade de gravar informações do cliente, 
+            //passando um objeto cliente como parâmetro
+        }
