adfbeb8e6de7503a3bb658b0b352a5b2b261751a
RaulMelnek <miguel.rauldesouzasantos@gmail.com>
2025-09-22 21:39:48 -0300

    Adiciona modificadores 'final' a variáveis e organiza comentários sobre tipos primitivos

--- JavaBasico_Variaveis/src/estudo/JavaBasico/variáveis/JavaBasicoVariaveis.java
+++ JavaBasico_Variaveis/src/estudo/JavaBasico/variáveis/JavaBasicoVariaveis.java
@@ -4,12 +4,12 @@ public class JavaBasicoVariaveis {
      * @param args
      * @throws Exception
      */
-    public static void main(String[] args) throws Exception {
+    public static void main(final String[] args) throws Exception {
 
-        int idade = 20;
-        String nome = "João";
-        double salario = 2500.50;
-        boolean ativo = true;
+        final int idade = 20;
+        final String nome = "João";
+        final double salario = 2500.50;
+        final boolean ativo = true;
 
         System.out.println("Nome: " + nome);
         System.out.println("Idade: " + idade);
@@ -17,13 +17,16 @@ public class JavaBasicoVariaveis {
         System.out.println("Ativo: " + ativo);
 
     // Exemplos de tipos primitivos
-    byte idadeByte = 30; // -128 a 127
-    short populacaoCidade = 15000; // -32.768 a 32.767
-    long populacaoMundial = 8000000000L; // Sufixo L para long
-    float altura = 1.75f; // Sufixo f para float
-    char inicial = 'J'; // Um único caractere
-    double preco = 19.99; // Números decimais de dupla precisão
-    boolean aprovado = false; // true ou false
+    // tipos primitivos
+    //classes de texto string
+
+        final byte idadeByte = 30; // -128 a 127
+        final short populacaoCidade = 15000; // -32.768 a 32.767
+        final long populacaoMundial = 8000000000L; // Sufixo L para long
+        final float altura = 1.75f; // Sufixo f para float
+        final char inicial = 'J'; // Um único caractere
+        final double preco = 19.99; // Números decimais de dupla precisão
+        final boolean aprovado = false; // true ou false
 
     System.out.println("Byte: " + idadeByte);
     System.out.println("Short: " + populacaoCidade);
@@ -32,5 +35,9 @@ public class JavaBasicoVariaveis {
     System.out.println("Char: " + inicial);
     System.out.println("Double: " + preco);
     System.out.println("Boolean: " + aprovado);
+    
+    //constantes - sempre escritas em caixa alta e com 'final'
+        final double PI = 3.14;
+
     }
 }
