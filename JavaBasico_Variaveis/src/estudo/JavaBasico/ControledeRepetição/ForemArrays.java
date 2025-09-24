05de7c4072108ecc8524f33a0062efb2f803955e
RaulMelnek <miguel.rauldesouzasantos@gmail.com>
2025-09-24 14:21:26 -0300

    Refatora a classe 'ForemArrays' para utilizar um loop aprimorado e simplifica a exibição das marcas dos carros

--- JavaBasico_Variaveis/src/estudo/JavaBasico/ControledeRepetição/ForemArrays.java
+++ JavaBasico_Variaveis/src/estudo/JavaBasico/ControledeRepetição/ForemArrays.java
@@ -4,9 +4,9 @@ public class ForemArrays {
     // em arrays o indice começa em 0
     //se utiliza o .length para saber o tamanho do array
     public static void main(String[] args) {
-        String[] carros = {"NA","Volvo", "BMW", "Ford", "Mazda"};
-        for (int i = 0; i < carros.length; i++) {
-            System.out.println("O carro no indice " + i + " é: " + carros[i]);
+        String[] carros = {"Volvo", "BMW", "Ford", "Mazda"};
+        for(String carro : carros){
+            System.out.println("Marca do carro é:" + carro);
         }
     }
 }
